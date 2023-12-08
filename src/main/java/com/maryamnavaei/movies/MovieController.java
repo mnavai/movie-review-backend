a84b0b78dcc122ec0afe949d2a8c22ef4dbd6544
Maryam Navaei <maryam.navaei@gmail.com>
2023-12-07 21:54:43 -0500

    API added

--- src/main/java/com/maryamnavaei/movies/MovieController.java
+++ src/main/java/com/maryamnavaei/movies/MovieController.java
@@ -1,4 +1,15 @@
 package com.maryamnavaei.movies;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //API
+@RequestMapping("/api/v1/movies")
 public class MovieController {
+    @GetMapping
+    public String allMovies(){
+        return "All Movies";
+    }
+
 }
