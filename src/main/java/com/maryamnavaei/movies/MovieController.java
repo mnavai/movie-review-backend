c3298797460f6f25e2a972ffb9c4e6b5b790c1d8
Maryam Navaei <maryam.navaei@gmail.com>
2024-01-02 17:07:34 -0500

    Update MovieController.java

--- src/main/java/com/maryamnavaei/movies/MovieController.java
+++ src/main/java/com/maryamnavaei/movies/MovieController.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 
 @RestController //API
 @RequestMapping("/api/v1/movies")
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin("*")
 public class MovieController {
     @Autowired
     private MovieService movieService;
